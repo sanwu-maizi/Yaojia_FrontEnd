1da13448abe6095ba2a6e8d36a80cb01ca58b1f1
RuiQI Feng <134840145+AsterismF@users.noreply.github.com>
2024-11-21 10:40:36 +0800

    初步页面展示

---
+++ src/constants/index.ts
@@ -0,0 +1,2 @@
+export const MAX_LENGTH = 100;
+
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,20 @@
+import { memo, useState } from 'react';
+import './Login.css';
 function Login() {
+  const BUTTON_TEXT = '立即登录';
+  const [isLogin, setIsLogin] = useState(false);
+  const handleClick = () => {
+    setIsLogin(true);
+  }
   return (
-    <div>
-      11111
+    <div className='page' onClick={handleClick}>
+      <div className='login'>
+        <div className='login_title'>登录</div>
+        <button className='login_button'>{BUTTON_TEXT}</button>
+      </div>
+      {isLogin && <div className='login_success'>登录成功</div>}
     </div>
   )
-};
+}
 
-export default Login;
+export default memo(Login);
